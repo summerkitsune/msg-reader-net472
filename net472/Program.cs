880895303865f1c30d1001ba85c3654ebf9860e7
Julie Clarebots <julie.clarebots@gmail.com>
2024-03-27 11:58:42 +0100

    Initial Commit

---
+++ net472/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace net472
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fs = System.IO.File.Open("testmessage.msg", FileMode.Open))
+            {
+                using (var b = new BinaryReader(fs))
+                {
+                    byte[] blob = b.ReadBytes((int)fs.Length);
+                    using (var msg = new MsgReader.Outlook.Storage.Message(new MemoryStream(blob)))
+                    {
+                        Console.WriteLine(msg.Subject);
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
